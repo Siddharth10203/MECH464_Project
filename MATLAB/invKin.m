1a7b614bfcdec94922c55744a27443e37bdd0e8b
ryanschmidt604 <ryan.schmidt.604@gmail.com>
2019-03-29 15:22:10 -0700

    inv kin changed

--- MATLAB/invKin.m
+++ MATLAB/invKin.m
@@ -1,50 +1,19 @@
-function [arm_angles] = invKin(xyzcoords, pixel_space)
-% takes xyz coordinates and converts to 4 joint angles for the mearm.
-% norm is the max x or y in pixel space. the space should be a square with
-% range -x,+x. norm is a single value
-%% convert from pixel space to real space in mm.
-l1=100; %mm
-l2=100; %mm
-max_distance_real = l1+l2;
-scale=max_distance_real/pixel_space;
-x=xyzcoords(1)*scale;
-y=xyzcoords(2)*scale;
-z=xyzcoords(3)*scale;
+function [arm_angles] = invKin(r, theta, z)
+% takes cylindrical coordinates and converts to 3 joint angles for the mearm.
+l1=80; %mm
+l2=80; %mm
 
-xyplane_d=norm([x,y]);
-xyz_d=norm([x,y,z]);
-
-if xyz_d > max_distance_real
-    arm_angles=[0,0,0,0];
-    disp('coordinates outside workspace');
-    return
-end
 %% find theta_1
-temp = atan(y/x);
-
-if x>=0
-    theta_1=temp;
-    if y<0
-       theta_1=(2*pi)+temp; 
-    end
-else
-    theta_1=(pi)+temp;
-end
+theta_1 = theta;
 
 %% find theta_2, 3
-d = sqrt(x^2 + y^2);
-l = sqrt(d^2 + z^2);
+l = sqrt(r^2 + z^2);
 
-if z >= 0
-    theta_3 = acos((l^2-l1^2-l2^2)/(2*l1*l2));
-    alpha=atan(z/d);
-    beta= acos((l1^2+l^2-l2^2)/(2*l1*l));
-    theta_2 = alpha+beta;
-    
-else%TODO
-    theta_2 = 0;
-    theta_3 = 0;
-end
+temp_angle = acos((l^2-l1^2-l2^2)/(2*l1*l2));
+alpha=atan2(z,d);
+beta= acos((l1^2+l^2-l2^2)/(2*l1*l));
+theta_2 = alpha+beta;
+theta_3 = temp_angle-theta_2;
 %% output values
 
 arm_angles = [theta_1 theta_2 theta_3];
